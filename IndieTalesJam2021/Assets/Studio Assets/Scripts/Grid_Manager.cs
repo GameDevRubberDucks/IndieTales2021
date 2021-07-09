98c36e804ed33b47be09278eed60f57fd56b3fcb
Dan <dmaccormick@hotmail.com>
2021-07-09 17:56:40 -0400

    Created the basics of the grid layout system

---
+++ IndieTalesJam2021/Assets/Studio Assets/Scripts/Grid_Manager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using RubberDucks.Util;
+
+public class Grid_Manager : MonoBehaviour
+{
+    //--- Public Variables ---//
+    [Header("Grid Spawning")]
+    public Vector2Int m_gridDimensions;
+    public Vector2Int m_gridBoundaryDimensions;
+    public int m_gridZPos;
+    public Transform m_gridCellParent;
+    public bool m_regenOnStart;
+
+    [Header("Cells")]
+    public GameObject m_gridCellPrefab;
+    public GameObject m_gridBoundaryPrefab;
+
+
+
+    //--- Private Variables ---//
+    private Grid m_grid;
+
+
+
+    //--- Unity Methods ---//
+    private void Awake()
+    {
+        // Init the private variables
+        m_grid = GetComponent<Grid>();
+        var go = new GameObject();
+    }
+
+    private void Start()
+    {
+        // Generate the grid
+        if (m_regenOnStart)
+            GenerateGrid();
+    }
+
+
+
+    //--- Public Methods ---//
+    [ContextMenu("Generate Grid")]
+    public void GenerateGrid()
+    {
+        // First, clear any existing tiles
+        ClearGrid();
+
+        // Ensure the grid component has been assigned
+        if (!m_grid)
+            m_grid = GetComponent<Grid>();
+
+        // Now, place the tiles into the world using the underlying grid information
+        int halfWidth = m_gridDimensions.x / 2;
+        int halfHeight = m_gridDimensions.y / 2;
+        for(int row = (-halfHeight - m_gridBoundaryDimensions.y); row <= (halfHeight + m_gridBoundaryDimensions.y); row++)
+        {
+            for (int col = (-halfWidth - m_gridBoundaryDimensions.x); col <= (halfWidth + m_gridBoundaryDimensions.x) ; col++)
+            {
+                // Determine if the cell being spawned is actually one of the boundary blocks
+                bool isBoundary = (row < -halfHeight || row > halfHeight) ||
+                                   (col < -halfWidth || col > halfWidth);
+                // Spawn the cell
+                GameObject cellPrefab = (isBoundary) ? m_gridBoundaryPrefab : m_gridCellPrefab;
+                var newCell = Instantiate(cellPrefab, m_gridCellParent);
+                var cellNameStart = isBoundary ? "Boundary" : "Cell";
+                newCell.name = cellNameStart + " [Row = " + row.ToString() + "][Col = " + col.ToString() + "]";
+                newCell.transform.position = m_grid.GetCellCenterWorld(new Vector3Int(col, row, m_gridZPos));
+            }
+        }
+    }
+
+    public void ClearGrid()
+    {
+        // Delete all of the grid cells by removing any children from the grid parent
+        Util_ClearChildren.ClearChildren(m_gridCellParent, true);
+    }
+}
---
+++ IndieTalesJam2021/Assets/Studio Assets/Scripts/Util/Util_ClearChildren.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace RubberDucks.Util
+{
+    public class Util_ClearChildren : MonoBehaviour
+    {
+        //--- Static Methods ---//
+        public static void ClearChildren(Transform _transform, bool _destroyImmediate = false)
+        {
+            // Destroy the normal method or destroy immediate (immediate is usually used in editor)
+            if (_destroyImmediate)
+            {
+                // Immediate destruction removes the child from the list of children right away
+                // So, we need to keep looping until the list is empty
+                // Otherwise, we'll only remove half of the children since the childCount variable will keep decreasing
+                while (_transform.childCount > 0)
+                    DestroyChild(_transform, 0, true);
+            }
+            else
+            {
+                // Normal destruction does NOT remove the child from the list right away
+                // So, we need to iterate through the list fully
+                // Waiting until the list is empty will result in an infinite loop
+                for (int i = 0; i < _transform.childCount; i++)
+                    DestroyChild(_transform, i, false);
+            }
+        }
+
+        public static void ClearChildren(GameObject _gameObject, bool _destroyImmediate = false)
+        {
+            ClearChildren(_gameObject.transform, _destroyImmediate);
+        }
+
+
+
+        //--- Utility Methods ---//
+        private static void DestroyChild(Transform _parent, int _childIndex, bool _destroyImmediate)
+        {
+            // Get the object reference
+            GameObject child = _parent.GetChild(_childIndex).gameObject;
+
+            // Use the correct destruction method, either the immediate version or the default
+            if (_destroyImmediate)
+                DestroyImmediate(child);
+            else
+                Destroy(child);
+        }
+    }
+}
